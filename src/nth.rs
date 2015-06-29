5b72b7668f5a2a3fd5df1855fa750022b75645d3
Cole Thompson <cole.thompson@gmail.com>
2015-06-28 20:08:58 -0700

    repace regex with split_whitespace

--- src/nth.rs
+++ src/nth.rs
@@ -4,7 +4,6 @@ use std::env;
 use std::io::*;
 use std::process::exit;
 use std::str::FromStr;
-use regex::Regex;
 
 fn main() {
   let arguments: Vec<_> = env::args().collect();
@@ -36,15 +35,13 @@ fn usage_err_exit() {
 }
 
 fn handle_input(column_numbers: &Vec<usize>) {
-  let splitter = Regex::new(r"\s+").unwrap();
-
   let mut input = stdin();
   let mut read_buf: String = String::new();
   let mut line = input.read_line(&mut read_buf);
 
   while line.is_ok() && line.unwrap() > 0 {
     let line_val = read_buf.clone();
-    let column_vals: Vec<&str> = splitter.split(&line_val).collect();
+    let column_vals: Vec<&str> = line_val.split_whitespace().collect();
 
     handle_line(column_vals, &column_numbers);
 
