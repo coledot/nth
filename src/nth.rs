e2ff2c6734e7a9b37f8666b394b965be6fffc86c
Cole Thompson <cole.thompson@gmail.com>
2015-06-29 18:56:16 -0700

    use buffered writes for better performance

--- src/nth.rs
+++ src/nth.rs
@@ -1,5 +1,5 @@
 use std::env;
-use std::io::{stdin, Write, stderr, Error};
+use std::io::{stderr, stdin, stdout, BufWriter, Error, Stdout, Write};
 use std::process::exit;
 
 fn main() {
@@ -18,8 +18,8 @@ fn parse_args() -> Vec<usize> {
   env::args().skip(1).map(|x| {
     match x.parse::<isize>() {
       Ok(val) if val < 1 => usage_err_exit(Some("Column number must be a positive integer".to_string())),
-      Ok(val) => val as usize,
-      Err(err) => usage_err_exit(Some(format!("Failed to parse '{}' with error: {}", x, err))),
+      Ok(val)            => val as usize,
+      Err(err)           => usage_err_exit(Some(format!("Failed to parse '{}' with error: {}", x, err))),
     }
   }).collect()
 }
@@ -34,6 +34,7 @@ fn usage_err_exit(msg: Option<String>) -> ! {
 
 fn handle_input(column_numbers: &[usize]) -> Result<(), Error> {
   let mut input = stdin();
+  let mut out_buf = BufWriter::new(stdout());
   let mut read_buf = String::new();
 
   loop {
@@ -42,19 +43,18 @@ fn handle_input(column_numbers: &[usize]) -> Result<(), Error> {
       return Ok(())
     }
 
-      
-    handle_line(&read_buf, &column_numbers);
+    handle_line(&mut out_buf, &read_buf, &column_numbers);
     read_buf.clear();
   }
 }
 
-fn handle_line(read_buf: &String, column_numbers: &[usize]) {
+fn handle_line(out_buf: &mut BufWriter<Stdout>, read_buf: &String, column_numbers: &[usize]) {
   let column_vals: Vec<&str> = read_buf.split_whitespace().collect();
   for col_num in column_numbers.iter() {
     if let Some(val) = column_vals.get(col_num - 1) {
-      print!("{} ", val);
+      write!(out_buf, "{} ", val).unwrap();
     }
   }
-  println!("");
+  writeln!(out_buf, "").unwrap();
 }
 
